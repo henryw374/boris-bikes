4e11cf967755ff3793b90ea73be62b5689786f90
henryw374 <henryw374@gmail.com>
2018-02-03 08:40:26 +0000

    no auth api working

--- src/leyton_boris_bikes/bikes.clj
+++ src/leyton_boris_bikes/bikes.clj
@@ -7,22 +7,53 @@
   (Math/sqrt
     (+
       (Math/pow (- (:lat a) (:lat b)) 2)
-      (Math/pow (- (:long a) (:long b)) 2))))
+      (Math/pow (- (:lon a) (:lon b)) 2))))
 
-(def centre-location {:lat 51.561948 :long -0.013139})
+(def centre-location {:lat 51.561948 :lon -0.013139})
 (def max-bike-points 5)
-(def tfl-bike-resource "https://api.tfl.gov.uk/bikepoint")
+(def tfl-host "https://api.tfl.gov.uk")
+(def tfl-bike-resource (str tfl-host "/bikepoint"))
 
 (defn bike-data []
-  (try
-    (let [{:keys [status body] :as response} (client/get tfl-bike-resource {:timeout 2})]
-      (if (= 200 status)
-        {:data body}
-        (throw (ex-info "failed to get bike data" response))))
-    (catch Exception e
-      (log/info e)
-      {:data "resources/sample-bike-data.json"})))
+  (->
+    (try
+      (let [{:keys [status body] :as response} (client/get tfl-bike-resource {:timeout 2})]
+        (if (= 200 status)
+          {:data body
+           :status :fresh}
+          (throw (ex-info "failed to get bike data" response))))
+      (catch Exception e
+        (log/info e)
+        {:data (slurp "resources/sample-bike-data.json")
+         :status :stale}))
+    (update :data cheshire/parse-string true)))
+
+(defn number-of-available-bikes [bike-point]
+  (->> bike-point
+       :additionalProperties
+       (filter #(= "NbBikes" (:key %)))
+       first
+       :value))
+
+(defn shape-for-client [bike-point]
+  (-> bike-point
+      (select-keys [:distance
+                    :lon 
+                    :url 
+                    :lat 
+                    :commonName ])
+      (update :url #(str tfl-host %))
+      (assoc :available-bikes (number-of-available-bikes bike-point))))
+
+(defn closest-bike-points [{:keys [data]}]
+  (->>
+    data
+    (mapv #(assoc % :distance (distance centre-location %)))
+    (sort-by :distance)
+    (take max-bike-points)
+    (mapv shape-for-client)))
 
 
 (defn bike-points-with-availability []
-  )
+  (-> (bike-data)
+      (closest-bike-points)))
--- test/leyton_boris_bikes/bikes_test.clj
+++ test/leyton_boris_bikes/bikes_test.clj
@@ -6,4 +6,4 @@
 (deftest distance-between-2-points
   (testing "3,4,5 triangle"
     (is
-      (= 5E0 (sut/distance {:lat 0 :long 0} {:lat 3 :long 4})))))
+      (= 5E0 (sut/distance {:lat 0 :lon 0} {:lat 3 :lon 4})))))
